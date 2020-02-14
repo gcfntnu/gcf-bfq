299da6e6e3c7d666ef58a59cef3c574ffb74d3fb
Geir A <geiramh@stud.ntnu.no>
2020-02-14 09:33:06 +0100

    update demultiplex qiaseq -- fixes bug where region V3V4 does not demultiplex when primers are read from file. Fixed by reading primers serially and extracting from the remaining reads that has not hit any previous regions

--- bcl2fastq_pipeline/bcl2fastq_pipeline/makeFastq.py
+++ bcl2fastq_pipeline/bcl2fastq_pipeline/makeFastq.py
@@ -194,13 +194,31 @@ def demultiplex_qiaseq(config):
 
 def cutadapt_worker(fname):
     sample = os.path.basename(fname).replace("_R1.fastq.gz","")
+    forward = pd.read_csv("/opt/qiaseq/qiaseq_primers_fwd.csv", index_col=0)
+    reverse = pd.read_csv("/opt/qiaseq/qiaseq_primers_rev.csv", index_col=0)
 
-    cmd = "/opt/conda/bin/cutadapt -g file:/opt/qiaseq/qiaseq_primers_fwd.fa -G file:/opt/qiaseq/qiaseq_primers_rev.fa --pair-adapters --no-indels -e 0.1 --suffix ':region={{name}}' -o {sample}_{{name}}_R1.fastq -p {sample}_{{name}}_R2.fastq {r1} {r2} > log/{sample}_qiaseq_demultiplex.log".format(
-        sample = sample,
-        r1 = fname,
-        r2 = fname.replace("R1.fastq.gz","R2.fastq.gz")
-        )
-    subprocess.check_call(cmd, shell=True)
+    for i, r in forward.iterrows():
+        if os.path.exists("{}_unknown_R1.fastq".format(sample)):
+            fname = "{}_unknown_R1.fastq".format(sample)
+            cp_cmd = "cp -f {} input_{}".format(fname, fname)
+            subprocess.check_call(cp_cmd, shell=True)
+            cp_cmd = "cp -f {} input_{}".format(fname.replace("R1.fastq","R2.fastq"), fname.replace("R1.fastq","R2.fastq"))
+            subprocess.check_call(cp_cmd, shell=True)
+
+        cmd = "cutadapt -g {region}={fwd_primer} -G {region}={rev_primer} --pair-adapters --no-indels -e 0.1 --untrimmed-output {unknown_r1} --untrimmed-paired-output {unknown_r2} --suffix ':region={{name}}' -o {sample}_{{name}}_R1.fastq -p {sample}_{{name}}_R2.fastq {r1} {r2} >> log/{sample}_qiaseq_demultiplex.log".format(
+            sample = sample,
+            unknown_r1 = "{}_unknown_R1.fastq".format(sample),
+            unknown_r2 = "{}_unknown_R2.fastq".format(sample),
+            region = i,
+            fwd_primer = r['primer'],
+            rev_primer = reverse.loc[i, 'primer'],
+            r1 = ("input_" + fname) if "unknown_R1.fastq" in fname else fname,
+            r2 = ("input_" + fname.replace("R1.fastq", "R2.fastq")) if "unknown_R1.fastq" in fname else fname.replace("R1.fastq", "R2.fastq")
+            )
+        subprocess.check_call(cmd, shell=True)
+
+    rm_cmd = "rm input_{}_*fastq".format(sample)
+    subprocess.check_call(rm_cmd, shell=True)
 
     R1 = glob.glob("{}*R1.fastq".format(sample))
     r1 = " ".join(R1)
